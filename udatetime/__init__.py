0d709b0d8f365fa710159623578934ff85f23a27
Simon Pirschel <simon@aboutsimon.com>
2016-08-02 15:21:47 +0200

    use pure python for python3 and pypy

--- udatetime/__init__.py
+++ udatetime/__init__.py
@@ -7,7 +7,18 @@ except ImportError:
     __pypy__ = None
 
 if __pypy__ or sys.version_info.major == 3:
-    from udatetime._ffi import (
+    # from udatetime._ffi import (
+    #     utcnow,
+    #     now,
+    #     from_rfc3339_string as from_string,
+    #     to_rfc3339_string as to_string,
+    #     utcnow_to_string,
+    #     now_to_string,
+    #     from_timestamp as fromtimestamp,
+    #     from_utctimestamp as utcfromtimestamp,
+    #     TZFixedOffset
+    # )
+    from udatetime._pure import (
         utcnow,
         now,
         from_rfc3339_string as from_string,
